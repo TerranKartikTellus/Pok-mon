6d6d7a7cc008e3a0b6f0706725eba1977034096d
TarranKartikTellus <tarrankartiktellus@gmail.com>
2021-12-24 17:32:29 +0530

    bg gray + text white

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import "tailwindcss/tailwind.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return ( 
-  <div>
+  <div className='bg-neutral-900 text-neutral-50 min-w-full min-h-screen m-0 p-0'>
       <Component {...pageProps} />
       <footer className=""></footer>
   </div>
